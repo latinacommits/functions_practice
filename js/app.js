ada63dc4a919121e65e52d21850b363663a0e123
Jessica <jjsepulveda91@gmail.com>
2023-02-01 22:01:22 -0500

    printGreeting

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// ### 1. printGreeting
+
+// Write a function called `printGreeting` with a parameter `name` that returns a greeting with the argument **interpolated** into the greeting.
+
+function printGreeting (name){
+    return("Hello there " + name );
+}
+console.log(printGreeting("Slimer"));
+
+// > => Hello there, Slimer!
